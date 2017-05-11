a5d2723a1989695f769b7a6a84e86282d2b5fac0
dpfaffenbauer <dominik@lineofcode.at>
2017-05-11 18:25:31 +0200

    [Refactor] move all to src/CoreShop folder

---
+++ Doctrine/ORM/CountryRepository.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Bundle\AddressBundle\Doctrine\ORM;
+
+use CoreShop\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use CoreShop\Component\Address\Repository\CountryRepositoryInterface;
+
+class CountryRepository extends EntityRepository implements CountryRepositoryInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function createListQueryBuilder()
+    {
+        return $this->createQueryBuilder('o');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findByName($name, $locale)
+    {
+        return $this->createQueryBuilder('o')
+            ->innerJoin('o.translations', 'translation')
+            ->andWhere('translation.name = :name')
+            ->andWhere('translation.locale = :locale')
+            ->setParameter('name', $name)
+            ->setParameter('localeCode', $locale)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findByCode($code)
+    {
+        return $this->createQueryBuilder('o')
+            ->setParameter('isoCode', $code)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
---
+++ Form/Type/CountryType.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Bundle\AddressBundle\Form\Type;
+
+use CoreShop\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class CountryType extends AbstractResourceType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('isoCode', TextType::class)
+            ->add('active', CheckboxType::class)
+            ->add('zone', ZoneChoiceType::class, [
+                'active' => null,
+            ])
+            ->add('addressFormat', TextareaType::class)
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'coreshop_country';
+    }
+}
