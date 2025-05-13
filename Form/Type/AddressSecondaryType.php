d0b4b55384150d82db149eed54ff4251e54b021a
Krikochoritis Ioannis <krikochoritis@cors.gmbh>
2025-05-13 12:05:11 +0200

    [CORS-49-4.0] - create customer invoice - secondary address

---
+++ Form/Type/AddressSecondaryType.php
@@ -0,0 +1,102 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * CoreShop
+ *
+ * This source file is available under two different licenses:
+ *  - GNU General Public License version 3 (GPLv3)
+ *  - CoreShop Commercial License (CCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) CoreShop GmbH (https://www.coreshop.com)
+ * @license    https://www.coreshop.com/license     GPLv3 and CCL
+ *
+ */
+
+namespace CoreShop\Bundle\AddressBundle\Form\Type;
+
+use CoreShop\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class AddressSecondaryType extends AbstractResourceType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('company', TextType::class, [
+                'label' => 'coreshop.form.address.company',
+                'required' => false,
+            ])
+            ->add('salutation', SalutationChoiceType::class, [
+                'label' => 'coreshop.form.address.salutation',
+                'required' => false,
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'coreshop.form.address.firstname',
+                'required' => false,
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'coreshop.form.address.lastname',
+                'required' => false,
+            ])
+            ->add('street', TextType::class, [
+                'label' => 'coreshop.form.address.street',
+                'required' => false,
+            ])
+            ->add('number', TextType::class, [
+                'label' => 'coreshop.form.address.number',
+                'required' => false,
+            ])
+            ->add('postcode', TextType::class, [
+                'label' => 'coreshop.form.address.postcode',
+                'required' => false,
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'coreshop.form.address.city',
+                'required' => false,
+            ])
+            ->add('country', CountryChoiceType::class, [
+                'active' => true,
+                'label' => 'coreshop.form.address.country',
+                'required' => false,
+            ])
+            ->add('state', StateChoiceType::class, [
+                'active' => true,
+                'label' => 'coreshop.form.address.state',
+                'required' => false,
+            ])
+            ->add('phoneNumber', TextType::class, [
+                'label' => 'coreshop.form.address.phone_number',
+                'required' => false,
+            ])
+            ->add('_redirect', HiddenType::class, [
+                'mapped' => false,
+            ])
+        ;
+
+        if ($options['show_address_identifier_choice'] === true) {
+            $builder->add('addressIdentifier', AddressIdentifierChoiceType::class, [
+                'label' => false,
+                'required' => false,
+            ]);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        parent::configureOptions($resolver);
+
+        $resolver->setDefault('show_address_identifier_choice', false);
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'coreshop_address';
+    }
+}
